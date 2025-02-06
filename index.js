e6812751b73f266430b9a2f594e57c96ceed9e10
Lewis Mander <lewis.mander@googlemail.com>
2025-02-06 13:05:57 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,14 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// enable CORS
 // API is remotely testable by FCC
 var cors = require('cors');
 const { application } = require('express');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));
 
-// http://expressjs.com/en/starter/static-files.html
-//app.use(express.static('public'));//
 app.use(express.static(path.join(__dirname, 'public')));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
 res.sendFile(__dirname + '/views/index.html');
 });
